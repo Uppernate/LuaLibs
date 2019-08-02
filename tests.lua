c7f968f741cbb982e1259cfe9fb3688161c65a92
Uppernate <uppernatearts@gmail.com>
2019-08-02 14:21:34 +0100

    More iterations

--- tests.lua
+++ tests.lua
@@ -3,7 +3,7 @@ local vector2 = require'./Vector.lua'
 
 local var
 local t = chronos.nanotime()
-for i = 1, 1000000 do
+for i = 1, 10000000 do
 	var = vector2()
 end
 t = (chronos.nanotime() - t) * 1000
@@ -12,7 +12,7 @@ print(string.format('vector2(), no parameters, %f', t))
 
 local var
 t = chronos.nanotime()
-for i = 1, 1000000 do
+for i = 1, 10000000 do
 	var = vector2(0, 0)
 end
 t = (chronos.nanotime() - t) * 1000
@@ -22,7 +22,7 @@ print(string.format('vector2(), number parameters, %f', t))
 local var
 local v = {0, 0}
 t = chronos.nanotime()
-for i = 1, 1000000 do
+for i = 1, 10000000 do
 	var = vector2(v)
 end
 t = (chronos.nanotime() - t) * 1000
